592af27dd39ab1c607a4a2dbecdffa6b42414b40
Ilke Zilci <ilke.zilci@fokus.fraunhofer.de>
2014-09-27 13:20:47 +0200

    soft constraints optimization objective offer-demand

--- simple-cp-matcher/src/main/java/de/tub/fokus/sm/cp/csp/SolveAsMatrixEqualProblem.java
+++ simple-cp-matcher/src/main/java/de/tub/fokus/sm/cp/csp/SolveAsMatrixEqualProblem.java
@@ -53,33 +53,48 @@ public class SolveAsMatrixEqualProblem extends AbstractProblem {
 
 	}
 
-	/** Example input and output:	
-	int[] q1values = { 0, 1, 1 };
-	int[] q2values = { 100, 90, 10 };
+	/**This model's optimization objective is to minimize the violations on the element constraints,
+	 * (q1values[serviceIndex]!=qosdemand[0]).asBool() if the constraint is satisfied asBool returns 1
+	 * if the constraint is not satisfied asBool returns 0,
+	 * which is multiplied by the weight and added to violationSum
+	 * The solver returns the serviceIndex with the minimum violationSum.  
+	 *  Example input and output:
+	 *  int[] qosdemand={2,80,96}	
+	int[] q1values = { 0, 1, 2 };
+	int[] q2values = { 100, 90, 80 };
 	int[] q3values = { 92, 96, 95 };
 	int[][] qvalues = { q1values, q2values, q3values };
 	INFO: Solution #1:
 	 serviceIndex[1] Total Constraint Violations[80]
 	*/
+	// TODO expect weights for each qosdemand
+
 	public void buildModelSoftAsBool(int[] qosdemand, int[][] qvalues) {
 		int serviceIndexMax = qvalues[0].length - 1;
 		// DEFINE SERVICE QUERY AS CONSTRAINTS WITH POST METHOD
 		Var indexVar = matching.variable("serviceIndex", 0, serviceIndexMax);
-		int[] q1values = qvalues[0];
+		int[] q1values = qvalues[1];
 		Element e = new Element(q1values, indexVar, "!=", qosdemand[0]);
 		// shorthand for Constraint c=matching.add(e);
 		// Var violation =c.asBool.multiply(20);
 		Var violation1 = matching.add(e).asBool().multiply(20);
 
-		int[] q2values = qvalues[1];
+		int[] q2values = qvalues[2];
 		Element e2 = new Element(q2values, indexVar, "!=", qosdemand[1]);
 		Var violation2 = matching.add(e2).asBool().multiply(60);
 
-		int[] q3values = qvalues[2];
+		int[] q3values = qvalues[3];
 		Element e3 = new Element(q3values, indexVar, "!=", qosdemand[2]);
 		Var violation3 = matching.add(e3).asBool().multiply(120);
 
+		// just to confirm the statement: if optimization is on another
+		// variable, serviceIndex is not costrained and simply the first value
+		// in its domain is chosen:
+		// int[] elseDomain = { 99, 20, 47 };
+		// Var violationElse = matching.variable("violationElse", elseDomain);
+		// Var[] weightVars = { violationElse };
 		Var[] weightVars = { violation1, violation2, violation3 };
+
 		// Optimization objective
 		Var weightedSum = matching.sum(weightVars);
 		weightedSum.setName("Total Constraint Violations");
@@ -88,13 +103,11 @@ public class SolveAsMatrixEqualProblem extends AbstractProblem {
 		solution.log();
 	}
 
-	// gives correct values for each property but not the correct serviceIndex
-	// TODO implement the relation between serviceIndex and properties
 	public void buildModelSoftDifference(int[] qosdemand, int[][] qvalues)
 			throws ConstraintException {
 		int serviceIndexMax = qvalues[0].length - 1;
 		// DEFINE SERVICE QUERY AS CONSTRAINTS WITH POST METHOD
-		Var indexVar = matching.variable("serviceIndex", 0, serviceIndexMax);
+		Var indexVar = matching.variable("serviceIndex", 1, serviceIndexMax);
 		// create the same constraints as buildModelSoftAsBool
 		int[] serviceIds = qvalues[0];
 		Var serviceIDs = matching.variable("serviceId", serviceIds);
@@ -108,52 +121,83 @@ public class SolveAsMatrixEqualProblem extends AbstractProblem {
 		int[] q3values = qvalues[3];
 		Var q3property = matching.variable("property3", q3values);
 
-		Linear certainId = new Linear(serviceIDs, "=", serviceIds[0]);
-		Linear certainIndex = new Linear(indexVar, "=", 0);
-		Linear certainProperty1 = new Linear(q1property, "=", q1values[0]);
-
-		Constraint c1 = matching.linear(certainId.asBool(), "=",
-				certainIndex.asBool());
-		Constraint c12 = matching.linear(certainId.asBool(), "=",
-				certainProperty1.asBool());
-		Linear certainValue2 = new Linear(serviceIDs, "=", serviceIds[1]);
-		Linear certainIndex2 = new Linear(indexVar, "=", 1);
-		Constraint c2 = matching.linear(certainValue2.asBool(), "=",
-				certainIndex2.asBool());
-		Linear certainProperty2 = new Linear(q1property, "=", q1values[1]);
-		Linear certainValue3 = new Linear(serviceIDs, "=", serviceIds[2]);
-		Linear certainIndex3 = new Linear(indexVar, "=", 2);
-		Constraint c3 = matching.linear(certainValue3.asBool(), "=",
-				certainIndex3.asBool());
-		Linear certainProperty3 = new Linear(q1property, "=", q1values[2]);
-
-		Linear certainId2 = new Linear(serviceIDs, "=", serviceIds[1]);
-		Constraint c22 = matching.linear(certainId2.asBool(), "=",
-				certainProperty2.asBool());
-		Linear certainId3 = new Linear(serviceIDs, "=", serviceIds[2]);
-		Constraint c32 = matching.linear(certainId3.asBool(), "=",
-				certainProperty3.asBool());
-		c1.post();
-		c12.post();
-		c22.post();
-		c32.post();
-		c2.post();
-		c3.post();
 		Element e = new Element(q1values, indexVar, "!=", qosdemand[0]);
 		Var violationE1 = matching.add(e).asBool().multiply(20);
 		Element e2 = new Element(q2values, indexVar, "!=", qosdemand[1]);
-		Var violationE2 = matching.add(e2).asBool().multiply(20);
+		Var violationE2 = matching.add(e2).asBool().multiply(60);
 		Element e3 = new Element(q3values, indexVar, "!=", qosdemand[2]);
-		Var violationE3 = matching.add(e3).asBool().multiply(20);
+		Var violationE3 = matching.add(e3).asBool().multiply(120);
 
 		// not posting the elements resulted in serviceindex independent of the
 		// values that properties get it did not have the relation
 		// q1values[indexVar], so post them but optimize on violation
+		// this one did not work, because once the element constraints are
+		// posted
+		// if there is no perfect match, the solver throws an exception that
+		// there are no solutions to the problem. another option would be to
+		// post those constraints
+		// with a lower consistency level, however i think this is not
+		// implemented yet. i will try that out later and ask feldman
 
-		Var violation1 = serviceIDs.minus(qosdemand[0]).abs();
+		// i can add here multiply to tell the weights of different properties.
+		Var violation1 = q1property.minus(qosdemand[0]).abs();
 		violation1.setName("violation1");
 		matching.add(violation1);
 		Var[] weightVars = { violation1, violationE1, violationE2, violationE3 };
+		// Optimization objective
+		Var weightedSum = matching.sum(weightVars);
+		weightedSum.setName("Total Constraint Violations");
+		matching.add(weightedSum);
+		Solution solution = matching.getSolver().findOptimalSolution(
+				Objective.MINIMIZE, weightedSum);
+		solution.log();
+
+		/**
+		 * Example:
+		 * Var i = matching.variable("i", 0, 10); 
+		 * Var j = matching.variable("j", 0, 10);
+		 * Var k = i.minus(j);
+		 */
+		// this.createSolver();
+		// this.solveAll();
+	}
+
+	public void buildSimpleModelDifference(int[] qosdemand, int[][] qvalues)
+			throws ConstraintException {
+		int[] serviceIds = qvalues[0];
+		Var serviceIDs = matching.variable("serviceId", serviceIds);
+		int[] q1values = qvalues[1];
+		Var q1property = matching.variable("property1", q1values);
+		int[] q2values = qvalues[2];
+		Var q2property = matching.variable("property2", q2values);
+		int[] q3values = qvalues[3];
+		Var q3property = matching.variable("property3", q3values);
+
+		// if serviceId==0 then q1property must have the value q1values[0]
+		Linear certainId = new Linear(serviceIDs, "=", serviceIds[0]);
+		Linear certainProperty1 = new Linear(q1property, "=", q1values[0]);
+		Constraint c12 = matching.linear(certainId.asBool(), "=",
+				certainProperty1.asBool());
+		// if serviceId==1 then q1property must have the value q1values[1]
+		Linear certainId2 = new Linear(serviceIDs, "=", serviceIds[1]);
+		Linear certainProperty2 = new Linear(q1property, "=", q1values[1]);
+		Constraint c22 = matching.linear(certainId2.asBool(), "=",
+				certainProperty2.asBool());
+		// if serviceId==2 then q1property must have the value q1values[2]
+		Linear certainId3 = new Linear(serviceIDs, "=", serviceIds[2]);
+		Linear certainProperty3 = new Linear(q1property, "=", q1values[2]);
+		Constraint c32 = matching.linear(certainId3.asBool(), "=",
+				certainProperty3.asBool());
+
+		c12.post();
+		c22.post();
+		c32.post();
+
+		// i can add here multiply to tell the weights of different properties.
+		Var violation1 = q1property.minus(qosdemand[0]).abs().multiply(20);
+		violation1.setName("violation1");
+		matching.add(violation1);
+		Var[] weightVars = { violation1 };
 		// Optimization objective
 		Var weightedSum = matching.sum(weightVars);
 		weightedSum.setName("Total Constraint Violations");
@@ -177,9 +221,10 @@ public class SolveAsMatrixEqualProblem extends AbstractProblem {
 		// int[] q1values = { 3, 3, 0, 3, 3, 3, 2 };
 		// int[] q2values = { 120, 100, 110, 120, 120, 120, 120 };
 		// int[] q3values = { 98, 95, 95, 98, 98, 98, 98 };
-		int[] qosdemand = { 5, 100, 96 };
+
 		int[] serviceIds = { 0, 1, 2 };
-		int[] q1values = { 2, 7, 6 };
+		int[] qosdemand = { 2, 100, 96 };
+		int[] q1values = { 5, 4, 3 };
 		int[] q2values = { 100, 100, 100 };
 		int[] q3values = { 96, 96, 96 };
 		int[][] qvalues = { serviceIds, q1values, q2values, q3values };
@@ -222,7 +267,6 @@ public class SolveAsMatrixEqualProblem extends AbstractProblem {
 		// this method just gets the solver of the problem, it is created
 		// if not created before
 		this.createSolver();
-
 		return this.solveAll();
 	}
 
